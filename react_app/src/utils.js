64218f5ccf4cd31ddefbf9b2ccad4111ba4831e7
Lap343 <lap343@gmail.com>
2021-02-25 18:07:49 -0800

    pushing app

---
+++ react_app/src/utils.js
@@ -0,0 +1,25 @@
+const getMoviesByName(search) {
+    const baseUrl = 'http://www.omdb.com'
+    const apiKey = '34898ec0'
+    const url = '${baseUrl}/?apikey=${apiKey}&s={search}'
+
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+}
+
+const getMovieDetailsById = async (movieId) => {
+    const baseUrl = 'http://www.omdb.com'
+    const apiKey = ''
+    const url = '${baseUrl}/?apikey=${apiKey}&s={movieId}'
+
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+getMoviesByName('batman')
+    .then((data) => console.log(data))
+
+getMovieDetailsById('tt0372784')
+    .then((data) => console.log(data))
